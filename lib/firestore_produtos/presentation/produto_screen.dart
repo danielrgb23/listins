e97b87b710cc2229c92f39a703caba0c53e06eba
Daniel Augusto <danielrgb23@gmail.com>
2023-07-26 20:20:18 -0300

    config: config filter for price, amount and name

--- lib/firestore_produtos/presentation/produto_screen.dart
+++ lib/firestore_produtos/presentation/produto_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:listin/firestore_produtos/helpers/enum_order.dart';
 import 'package:uuid/uuid.dart';
 import '../../firestore/models/listin.dart';
 import '../model/produto.dart';
@@ -19,6 +20,9 @@ class _ProdutoScreenState extends State<ProdutoScreen> {
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  OrdemProduto ordem = OrdemProduto.name;
+  bool isDecrescente = false;
+
   @override
   void initState() {
     refresh();
@@ -28,7 +32,24 @@ class _ProdutoScreenState extends State<ProdutoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.listin.name)),
+      appBar: AppBar(
+        title: Text(widget.listin.name),
+        actions: [
+          PopupMenuButton(itemBuilder: (context) {
+            return [
+              const PopupMenuItem(
+                  value: OrdemProduto.name, child: Text('Ordernar por nome')),
+              const PopupMenuItem(
+                  value: OrdemProduto.amount,
+                  child: Text('Ordernar por quantidade')),
+              const PopupMenuItem(
+                value: OrdemProduto.price,
+                child: Text('Ordernar por preço'),
+              ),
+            ];
+          })
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showFormModal();
